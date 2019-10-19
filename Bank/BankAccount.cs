e467eb9955c0554fc856ed4d64a54da35be3249d
Shashank Kuchibhotla <skuchibhotla@outlook.com>
2019-10-19 02:14:08 -0400

    Corrected bug in tests

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -5,10 +5,14 @@ namespace Bank
     /// <summary>
     /// Bank account demo class.
     /// </summary>
+
     public class BankAccount
     {
         private readonly string m_customerName;
         private double m_balance;
+
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
 
         private BankAccount()
         {
@@ -33,14 +37,14 @@ namespace Bank
 
         public void Debit(double amount)
         {
-            if(amount > m_balance)
+            if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
-            if(amount < 0)
+            if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
             m_balance -= amount;    // intentionally incorrect code for tests. 
--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -6,6 +6,9 @@ namespace BankTests
     [TestClass]
     public class BankAccountTests
     {
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
         [TestMethod]
         public void Debit_WithValidAmount_UpdatesBalance()
         {
@@ -38,15 +41,44 @@ namespace BankTests
         }
 
         [TestMethod]
-        public void Debit_WhenAmountIsGreaterThanBalance_ShouldThrowArgumentOutOfRange()
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
         {
+            //// Arrange
+            //double beginningBalance = 11.99;
+            //double debitAmount = 20.0;
+            //BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //// Act
+            //try
+            //{
+            //    account.Debit(debitAmount);
+            //}
+            //catch (System.ArgumentOutOfRangeException e)
+            //{
+            //    // Assert
+            //    StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            //}
+
             // Arrange
             double beginningBalance = 11.99;
-            double debitAmount = 20.00;
-            BankAccount account = new BankAccount("Mr. Saul Goodman", beginningBalance);
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Piotr Zelinski", beginningBalance);
 
-            // Act and Assert
-            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch(System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            // Fail Assert at the end to handle the case where there is not exception thrown. 
+            Assert.Fail("The expected exception was not thrown.");
         }
+
     }
 }
